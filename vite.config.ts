ec7db586163df537be14fce4af1329a1df4a9a5c
Rebeccaaby <rebeccaaby21@gmail.com>
2026-04-17 17:35:31 -0400

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio_website_rebeccaA/",
   server: {
     host: "::",
     port: 8080,
